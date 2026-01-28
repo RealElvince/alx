7ae1e05e810cd1570eb9c84e47d1063317e0a5bc
RealElvince <obuongely@yahoo.com>
2026-01-28 09:33:11 +0300

    negative indexing

--- string.py
+++ string.py
@@ -1,4 +1,4 @@
-my_string = "Hello,World"
+my_string = "Hello, World"
 
 # access character using positive indexing
 first_character = my_string[0]
@@ -8,3 +8,12 @@ third_character = my_string[2]
 print("The first character is",":",first_character)
 print("The second character is",":",second_character)
 print("The third character is",":",third_character)
+
+# access characters using negative indexing 
+last_char = my_string[-1]
+second_char = my_string[-2]
+first_char = my_string[-12]
+
+print("The last character",":",last_char)
+print("The second last character",":",second_char)
+print("The first character",":",first_char)
