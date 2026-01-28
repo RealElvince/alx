31ad699890d826c75e489498880ee85680c883b3
RealElvince <obuongely@yahoo.com>
2026-01-28 08:16:56 +0300

    added total_expenses_dollars variable.

--- farm_management.py
+++ farm_management.py
@@ -1,6 +1,14 @@
+# total harvest in Kilograms
 total_kgs_wheat_harvest = 2050
 total_kgs_potato_harvest = 3600
 
 total_kgs_harvest = total_kgs_wheat_harvest + total_kgs_potato_harvest
 
 print("Total harvest in Kilograms:",total_kgs_harvest)
+
+# Total Expenses in dollars
+seed_expense = 650
+labour_expense = 3070
+
+total_expenses_dollars = seed_expense + labour_expense
+print("Total expenses in dollars:",total_expenses_dollars)
