a0e85661cb9e2d49385e4cc09a1a3e0889758c8d
RealElvince <obuongely@yahoo.com>
2026-01-28 08:26:14 +0300

    added total_profit_dollars variable

--- farm_management.py
+++ farm_management.py
@@ -1,4 +1,4 @@
-# total harvest in Kilograms
+# Challenge 1: total harvest in Kilograms
 total_kgs_wheat_harvest = 2050
 total_kgs_potato_harvest = 3600
 
@@ -6,9 +6,20 @@ total_kgs_harvest = total_kgs_wheat_harvest + total_kgs_potato_harvest
 
 print("Total harvest in Kilograms:",total_kgs_harvest)
 
-# Total Expenses in dollars
+# Challenge 2:Total Expenses in dollars
 seed_expense = 650
 labour_expense = 3070
 
 total_expenses_dollars = seed_expense + labour_expense
 print("Total expenses in dollars:",total_expenses_dollars)
+
+# Challenge 3 : Calculate total revenue in dollars and print the results
+price_per_kg_wheat = 2
+price_per_kg_potato = 1.4
+
+total_revenue_dollars = (total_kgs_wheat_harvest*price_per_kg_wheat) + (total_kgs_potato_harvest*price_per_kg_potato)
+print("Total revenue in dollars:",total_revenue_dollars)
+
+# challenge 4:Calculate total profit in dollars
+total_profit_dollars = total_revenue_dollars - total_expenses_dollars
+print("Total profit in dollars:",total_profit_dollars)
