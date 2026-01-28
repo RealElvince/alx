3ccfc6e6f52eac56d7846428ba983862761f9664
RealElvince <obuongely@yahoo.com>
2026-01-28 08:55:14 +0300

    added mixed_variable,perform int and float casting.

--- casting.py
+++ casting.py
@@ -6,3 +6,12 @@ variable_b = int(variable_b)
 
 result = variable_a + variable_b
 print("result",":",type(result))
+
+# Perform a casting on mixed_variable to obtain a float and an integer version. Print both results along with their data types.
+mixed_variable = 56.78
+
+float_version = float(mixed_variable)
+integer_version = int(float_version)
+
+print("float_version",":",type(float_version))
+print("integer_version",":",type(integer_version))
