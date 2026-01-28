55ef2ef0486b9e487fb3bf40945906f56cf17eca
RealElvince <obuongely@yahoo.com>
2026-01-28 09:04:24 +0300

    positive indexing of string characters

---
+++ string.py
@@ -0,0 +1,10 @@
+my_string = "Hello,World"
+
+# access character using positive indexing
+first_character = my_string[0]
+second_character = my_string[1]
+third_character = my_string[2]
+
+print("The first character is",":",first_character)
+print("The second character is",":",second_character)
+print("The third character is",":",third_character)
