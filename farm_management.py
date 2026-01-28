0ec0f223327a2bf65be7c64ded20575776628ca1
RealElvince <obuongely@yahoo.com>
2026-01-28 08:13:24 +0300

    added farm_management file

---
+++ farm_management.py
@@ -0,0 +1,6 @@
+total_kgs_wheat_harvest = 2050
+total_kgs_potato_harvest = 3600
+
+total_kgs_harvest = total_kgs_wheat_harvest + total_kgs_potato_harvest
+
+print("Total harvest in Kilograms:",total_kgs_harvest)
