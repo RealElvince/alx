cd277b16e8d687608727ac5f36421fc8ed519638
RealElvince <obuongely@yahoo.com>
2026-01-28 09:53:01 +0300

    added string replication variable

---
+++ string_manipulation.py
@@ -0,0 +1,13 @@
+# concatenation
+string_one = "Hello"
+string_two = "World"
+
+concat_string = string_one + " " + string_two
+
+print(concat_string)
+
+# string repliaction
+original_string = "Amazing"
+
+replicated_string = original_string*4
+print(replicated_string)
