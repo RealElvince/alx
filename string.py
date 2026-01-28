3c6d84eb218f45bc78b6ecaa31cd69bf34cfb979
RealElvince <obuongely@yahoo.com>
2026-01-28 09:46:28 +0300

    print position 1 to 4 characters in my_string.

--- string.py
+++ string.py
@@ -5,9 +5,12 @@ first_character = my_string[0]
 second_character = my_string[1]
 third_character = my_string[2]
 
+
+
 print("The first character is",":",first_character)
 print("The second character is",":",second_character)
 print("The third character is",":",third_character)
+print(my_string[1:4])
 
 # access characters using negative indexing 
 last_char = my_string[-1]
