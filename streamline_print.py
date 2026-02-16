cfe170f4b458abad2a9e78ef0adec4d986a7987c
RealElvince <obuongely@yahoo.com>
2026-02-16 18:05:22 +0300

    string concat

---
+++ streamline_print.py
@@ -0,0 +1,5 @@
+forest_name = 'Mau Forest'
+area = 400000
+forest_type = 'closed-canopy montane'
+
+print('The' +forest_name+ 'is the largest ' +forest_type+ ' forest ecosystem in East Africa, covering' +str(area)+ ' hectares.')
