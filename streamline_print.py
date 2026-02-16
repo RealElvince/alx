88585a49eed8bf6997d87fb5f4f5085941020169
RealElvince <obuongely@yahoo.com>
2026-02-16 18:11:02 +0300

    use format()

--- streamline_print.py
+++ streamline_print.py
@@ -3,3 +3,10 @@ area = 400000
 forest_type = 'closed-canopy montane'
 
 print('The' +forest_name+ 'is the largest ' +forest_type+ ' forest ecosystem in East Africa, covering' +str(area)+ ' hectares.')
+
+# format()
+
+growth_rate = 2.5
+forest_region = 'Amazon'
+
+print('The annual growth rate of trees in the {} is {} %.'.format(forest_region,growth_rate))
