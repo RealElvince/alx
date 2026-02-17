76cf1e34ca65cd24551ed6f179c66a910fa75622
RealElvince <obuongely@yahoo.com>
2026-02-17 11:08:22 +0300

    use set constructor to create a set from list

--- sets.py
+++ sets.py
@@ -3,3 +3,9 @@ carnivorous = {"Lion","Leopard","Hyena","Bear"}
 
 print(carnivorous)
 print(type(carnivorous))
+
+# create a set from a list using set constructor
+
+herbivorous = set(["Buffalo","Zebra","Antelope","Wildbeast"])
+
+print(herbivorous)
