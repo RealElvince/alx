2372e0e05c5761c08841ff48df7e778417f2551c
RealElvince <obuongely@yahoo.com>
2026-02-17 11:32:36 +0300

    seti intersection

--- set_operations.py
+++ set_operations.py
@@ -3,3 +3,7 @@ from sets import carnivorous,herbivorous
 # set union
 all_animals = carnivorous.union(herbivorous)
 print(all_animals)
+
+# set intersection
+intersect_animals = carnivorous.intersection(herbivorous)
+print(intersect_animals)
