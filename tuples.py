d437cfd494a68b5d2c51aec33c08949af1c1f487
RealElvince <obuongely@yahoo.com>
2026-02-17 07:55:29 +0300

    combine two tuples using concat +

--- tuples.py
+++ tuples.py
@@ -14,3 +14,14 @@ print(fruit)
 
 text_only = tree_tuple[1:3]
 print(text_only)
+
+fruit_summer = ("peach","apricot","plum")
+
+fruit_winter = ("lemon","orange","grapefruit")
+
+print(fruit_summer,fruit_winter)
+
+# combine two tuples using concat
+all_fruits = fruit_summer + fruit_winter
+
+print(all_fruits)
