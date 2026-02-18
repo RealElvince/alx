031a2010492b2951f40b763e081ede62f43be003
RealElvince <obuongely@yahoo.com>
2026-02-18 07:56:22 +0300

    created nested dictionary

---
+++ nested_dictionaries.py
@@ -0,0 +1,7 @@
+fynbo_families = {
+    'Erica':{'Cape Floristic Region':670,'Worldwide':4500},
+    'Protea':{'Cape Floristic Region':330,'Worldwide':1350},
+    'Citrus':{'Cape Floristic Region':273,'Worldwide':1650},
+     'Phylica':{'Cape Floristic Region':173,'Worldwide':900}
+   
+}
