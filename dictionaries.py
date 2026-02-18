e358d9c644694de5b29f864f6e5dcc2fa6e6118c
RealElvince <obuongely@yahoo.com>
2026-02-18 07:45:51 +0300

    update dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -7,3 +7,9 @@ print(type(first_dictionary))
 first_dictionary['first_key'] = 'first_value'
 
 print(first_dictionary)
+
+# update dictionary
+extra_dictionary = {"second_key":"second_value","third_key":"third_value"}
+first_dictionary.update(extra_dictionary)
+
+print(first_dictionary)
