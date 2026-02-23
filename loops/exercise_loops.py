9e5ebb4c56242ad4d21273e74de21f9fcfc6935c
RealElvince <obuongely@yahoo.com>
2026-02-23 19:34:23 +0300

    Exercise 2 while loop

--- loops/exercise_loops.py
+++ loops/exercise_loops.py
@@ -3,3 +3,11 @@
 animals = ['Great White Shark', 'Blue Whale', 'African Elephant', 'Bald Eagle', 'Orangutan', 'Tiger', 'Panda', 'Koala']
 for animal in animals:
     print(animal)
+
+
+# Exercise 2 : Use a while loop to print numbers from 0 to 4.
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1
