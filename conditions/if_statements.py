ec0f292a57b7c53cbdc5fa9de34d5b76a28c4578
RealElvince <obuongely@yahoo.com>
2026-02-23 09:56:37 +0300

    nested if statement

--- conditions/if_statements.py
+++ conditions/if_statements.py
@@ -1,3 +1,13 @@
 deforestation_rate = 0.25
 if deforestation_rate > 0.20:
     print("Critical deforestation level reached!")
+
+
+# Nested if statements
+
+deforestation_rate = 0.18
+protected_area = True
+
+if deforestation_rate > 0.15:
+    if protected_area:
+        print("Urgent action needed in protected area due to high deforestation rate!")
