c37630247fc4c90840015fa971bf1d1f887d660c
RealElvince <obuongely@yahoo.com>
2026-02-23 18:11:32 +0300

    exercise two

--- conditions/exercise_condition_statements.py
+++ conditions/exercise_condition_statements.py
@@ -13,3 +13,17 @@ elif individuals_in_wild <= 20000:
 
 else:
     print("Safe")
+
+
+# Exercise 2
+rainfall = 205
+population_density = 448
+
+if rainfall < 500 and population_density > 500:
+    print("Severe Scarcity")
+
+elif (500 <= rainfall <= 1000) and (200 <= population_density <= 500):
+    print("Moderate Scarcity")
+
+else:
+    print("No Scarcity")
