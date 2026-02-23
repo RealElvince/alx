c86a1cda98874ad65f1da79c9ff64ff2807840d0
RealElvince <obuongely@yahoo.com>
2026-02-23 20:10:53 +0300

    exercise 1 for loop

---
+++ loops/controlling_loops.py
@@ -0,0 +1,7 @@
+endangered_marine_species = ['Hawksbill Turtle', 'Vaquita', 'Blue Whale', 'Staghorn Coral', 'Green Turtle']
+endangered_count = 0
+
+for species in endangered_marine_species:
+    endangered_count  += 1
+
+print(f"Number of endangered marine species: {endangered_count}")
