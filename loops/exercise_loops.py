b3fc239b216c0c95d1c060b5ff7b07284e8e8717
RealElvince <obuongely@yahoo.com>
2026-02-23 19:43:08 +0300

    use of break in a for loop

--- loops/exercise_loops.py
+++ loops/exercise_loops.py
@@ -11,3 +11,10 @@ i = 0
 while i < 5:
     print(i)
     i += 1
+
+
+# Exercise 4 : Stop the loop once you find Orangutan in the animals list.
+for animal in animals:
+    if animal == "Orangutan":
+        break
+    print(animal)
