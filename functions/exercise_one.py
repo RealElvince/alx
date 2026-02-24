603335a612745dda2455e55194f11f8baa45be59
RealElvince <obuongely@yahoo.com>
2026-02-24 21:01:19 +0300

    a function to convert acres to hectares

--- functions/exercise_one.py
+++ functions/exercise_one.py
@@ -6,3 +6,24 @@ total_deforested_land = sum(deforested_land)
 
 print(carbon_emission_factor)
 print(total_deforested_land)
+
+
+# Exercise 2
+
+def acres_to_hectares(acres):
+    """
+    conversion: acres to hectares
+    paramters : acres 
+
+    constant : 0.404686
+    return acres * constant
+    
+    
+    """
+    conversion_rate = 0.404686
+    hectares = acres*conversion_rate
+
+    return hectares
+
+acres_to_hectares(50)
+
