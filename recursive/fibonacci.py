fd6df9ad2576e65efdba7581f665b99e07a8475a
RealElvince <obuongely@yahoo.com>
2026-03-02 14:29:39 +0300

    print list of fibonacci numbers

--- recursive/fibonacci.py
+++ recursive/fibonacci.py
@@ -7,3 +7,12 @@ def calculate_fibo(n):
     
     else:
         return calculate_fibo(n-1) + calculate_fibo(n-2)
+    
+
+
+n = 9
+fibo_list = []
+for i in range(n+1):
+    fibo_list.append(calculate_fibo(i))
+
+print(fibo_list)
