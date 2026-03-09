d755ee2263d3d2910100b756ead3c3b34f10c7ba
RealElvince <obuongely@yahoo.com>
2026-03-09 15:35:18 +0300

    print endangered species

--- learn_pandas/df.py
+++ learn_pandas/df.py
@@ -28,3 +28,22 @@ print(tree_df)
 
 pine_data = tree_df.loc['Pine'][['Max_Height_m', 'Growth_Rate_cm_per_year']]
 print(pine_data)
+
+
+# Loading data from a CSV file named 'Animals.csv' into a DataFrame
+animals_df = pd.read_csv('https://raw.githubusercontent.com/Explore-AI/Public-Data/master/Data/Python/Animals.csv')
+
+# Displaying the first 5 rows of the DataFrame
+print(animals_df.head())
+
+
+# Create a new DataFrame based on a condition
+Critically_endangered = animals_df[animals_df['Conservation Status'] == 'Critically Endangered']
+
+# Displaying the newly created DataFrame
+print(Critically_endangered)
+
+
+# vulnerable species
+endangered = animals_df[animals_df['Conservation Status'] == 'Endangered']
+print(endangered)
