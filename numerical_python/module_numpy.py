c5e173a59289dba24c6c1a9db0f9ea0a599114a3
RealElvince <obuongely@yahoo.com>
2026-03-09 14:24:53 +0300

    create array of ones

--- numerical_python/module_numpy.py
+++ numerical_python/module_numpy.py
@@ -6,3 +6,8 @@ ratings = np.array([[94, 89, 63, 45], [93, 92, 48, 23], [92, 94, 56, 98]])
 print(ratings)
 
 print(ratings.shape)
+
+# create array of ones
+array_ones = np.ones((2,2))
+
+print(array_ones)
