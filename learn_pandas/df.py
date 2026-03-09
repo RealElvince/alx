c4b976e25c292099a650299f03345b732e297909
RealElvince <obuongely@yahoo.com>
2026-03-09 15:26:45 +0300

    create a data frame called tree_df

--- learn_pandas/df.py
+++ learn_pandas/df.py
@@ -12,3 +12,15 @@ crop_df = pd.DataFrame(data=df)
 print(crop_df)
 
 print(crop_df.iloc[1:3])
+
+
+tree_array = np.array([[30, 25], [60, 30], [25, 20]])
+
+columns = ["Max_Height_m","Growth_Rate_cm_per_year"]
+
+index = ["Maple", "Pine","Birch"]
+
+
+tree_df = pd.DataFrame(data=tree_array,columns=columns,index=index)
+
+print(tree_df)
