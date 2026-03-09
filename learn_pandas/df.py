b21405433a6e11fd0fccfe8d37ec54abb660d85b
RealElvince <obuongely@yahoo.com>
2026-03-09 15:18:25 +0300

    create a pandas dataframe

---
+++ learn_pandas/df.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+
+df = {
+    "Crop":['Wheat', 'Corn', 'Rice', 'Soybean'],
+    "Year_per_Acre":[3, 4.5, 5, 2.8],
+    "Country":['USA', 'China', 'India', 'Brazil']
+
+}
+
+crop_df = pd.DataFrame(data=df)
+print(crop_df)
